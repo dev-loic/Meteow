09010c24b04a6e389837a6ed0b321cb0eac2e90a
loic.saillant <loic.saillant.dev@gmail.com>
2021-12-30 11:26:02 +0100

    Start configuring views

--- Classes/App/Search/UI/ViewController/SearchViewController.swift
+++ Classes/App/Search/UI/ViewController/SearchViewController.swift
@@ -12,14 +12,51 @@ import UIKit
 class SearchViewController: UIViewController {
     
     var presenter: SearchPresenter?
-
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+    private lazy var dataSource = self.createDataSource()
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
         presenter?.start()
+    }
+    
+    // MARK: - Private
+    
+    private func createDataSource() -> SearchDataSource {
+        let dataSource = SearchDataSource()
+        dataSource.delegate = self
+        return dataSource
+    }
+    
+    private func setUpViews() {
+        navigationItem.title = "search_navigation_bar_title".localized()
+        dataSource.registerCells(in: tableView)
+        tableView.dataSource = dataSource
+        tableView.delegate = dataSource
+        setUpSearchBar()
+    }
+    
+    private func setUpSearchBar() {
+        searchBar.placeholder = "search_bar_placeholder".localized()
+        searchBar.delegate = self
     }
 }
 
 extension SearchViewController: SearchViewContract {
     
     // MARK: - SearchViewContract
+}
+
+extension SearchViewController: SearchDataSourceDelegate {
+    
+    // MARK: - SearchDataSourceDelegate
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    // MARK: - UISearchBarDelegate
 }
