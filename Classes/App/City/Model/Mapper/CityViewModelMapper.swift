7be5a848b58f9b2f7486e9404ba42be10561ca08
loic.saillant <loic.saillant.dev@gmail.com>
2022-01-02 12:37:34 +0100

    Add celsiusValue to display temperature of one city

--- Classes/App/City/Model/Mapper/CityViewModelMapper.swift
+++ Classes/App/City/Model/Mapper/CityViewModelMapper.swift
@@ -14,6 +14,6 @@ class CityViewModelMapper {
     func map(data: [WeatherData]) -> CityViewModel {
         // TODO: (Loic Saillant) 2022/01/02 To complete
         guard data.count > 0 else { return .empty }
-        return CityViewModel(currentTemperature: String(data[0].temperature))
+        return CityViewModel(currentTemperature: String(data[0].temperature.celsiusValue))
     }
 }
