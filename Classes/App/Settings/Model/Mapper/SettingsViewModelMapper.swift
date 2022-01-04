5af179f5d5e1bfd60598a93f5068aa719c7f0e55
loic.saillant <loic.saillant.dev@gmail.com>
2022-01-04 12:44:51 +0100

    Don't display favorites section if no favorites

--- Classes/App/Settings/Model/Mapper/SettingsViewModelMapper.swift
+++ Classes/App/Settings/Model/Mapper/SettingsViewModelMapper.swift
@@ -10,8 +10,15 @@ class SettingsViewModelMapper {
     // MARK: - SettingsViewModelMapper
     
     func map(fromFavoriteCities favoriteCities: [City]) -> SettingsViewModel {
-        let cells = favoriteCities.map { SettingsCellViewModel(title: $0.name) }
-        let favoriteSection = SettingsSectionViewModel(title: "settings_favorites_section_title".localized(), cells: cells)
-        return SettingsViewModel(sections: [favoriteSection])
+        let sections = [favoriteSection(favoriteCities)].compactMap { $0 }
+        return SettingsViewModel(sections: sections)
+    }
+    
+    // MARK: - Private
+    
+    private func favoriteSection(_ cities: [City]) -> SettingsSectionViewModel? {
+        guard cities.count > 0 else { return nil }
+        let cells = cities.map { SettingsCellViewModel(title: $0.name) }
+        return SettingsSectionViewModel(title: "settings_favorites_section_title".localized(), cells: cells)
     }
 }
